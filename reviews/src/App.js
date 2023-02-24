feb2c4658726b0e1904e6cb14abca81e0336f523
warish <warination@gmail.com>
2023-02-25 03:08:40 +0530

    done

---
+++ reviews/src/App.js
@@ -0,0 +1,17 @@
+import Reviews from "./Reviews";
+
+
+function App() {
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Our Reviews</h2>
+        <div className="underline"></div>
+      </div>
+    <Reviews/>
+
+    </section>
+  );
+}
+
+export default App;
